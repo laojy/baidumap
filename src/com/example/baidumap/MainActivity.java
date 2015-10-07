2885f7c9cdfc83d723a87860c8c306385da7c4e5
laojy <272164084@qq.com>
2015-10-07 15:51:05 +0800

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   AndroidManifest.xml 	new file:   bin/AndroidManifest.xml 	new file:   bin/baidumap.apk 	new file:   bin/classes.dex 	new file:   bin/jarlist.cache 	new file:   bin/res/crunch/drawable-hdpi/ic_launcher.png 	new file:   bin/res/crunch/drawable-mdpi/ic_launcher.png 	new file:   bin/res/crunch/drawable-xhdpi/ic_launcher.png 	new file:   bin/res/crunch/drawable-xxhdpi/ic_launcher.png 	new file:   bin/resources.ap_ 	new file:   gen/com/example/baidumap/BuildConfig.java 	new file:   gen/com/example/baidumap/R.java 	new file:   ic_launcher-web.png 	new file:   libs/armeabi/libBaiduMapSDK_v3_5_0_31.so 	new file:   libs/armeabi/libapp_BaiduPanoramaAppLib.so 	new file:   libs/armeabi/liblocSDK6a.so 	new file:   proguard-project.txt 	new file:   project.properties 	new file:   res/drawable-hdpi/ic_launcher.png         new file:   res/drawable-mdpi/ic_launcher.png 	new file:   res/drawable-xhdpi/ic_launcher.png 	new file:   res/drawable-xxhdpi/ic_launcher.png 	new file:   res/layout/activity_main.xml 	new file:   res/menu/main.xml 	new file:   res/values-sw600dp/dimens.xml 	new file:   res/values-sw720dp-land/dimens.xml 	new file:   res/values-v11/styles.xml 	new file:   res/values-v14/styles.xml 	new file:   res/values/dimens.xml 	new file:   res/values/strings.xml         new file:   res/values/styles.xml 	new file:   src/com/example/baidumap/MainActivity.java

---
+++ src/com/example/baidumap/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.baidumap;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+  MapView mapview=null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SDKInitializer.initialize(getApplicationContext());
+		setContentView(R.layout.activity_main);
+		mapview=(MapView)super.findViewById(R.id.bmapView);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	@Override  
+    protected void onDestroy() {  
+        super.onDestroy();  
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
+        mapview.onDestroy();  
+    }  
+    @Override  
+    protected void onResume() {  
+        super.onResume();  
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
+        mapview.onResume();  
+        }  
+    @Override  
+    protected void onPause() {  
+        super.onPause();  
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
+        mapview.onPause();  
+        }  
+    
+
+}
